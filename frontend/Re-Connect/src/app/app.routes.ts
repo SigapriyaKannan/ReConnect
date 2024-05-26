99fc813526132ffb5bafcd02ac7e00dd9de1e1ec
Vineeth Gudipalli <vn388445@dal.ca>
2024-05-25 23:09:21 -0300

    feat: Initialize routing

--- frontend/Re-Connect/src/app/app.routes.ts
+++ frontend/Re-Connect/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/shared/login/login.component';
+import { SignupComponent } from './components/shared/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "login",
+        component: LoginComponent
+    },
+    {
+        path: "sign-up",
+        component: SignupComponent
+    },
+    {
+        path: "",
+        redirectTo: "login",
+        pathMatch: "full"
+    }
+];
