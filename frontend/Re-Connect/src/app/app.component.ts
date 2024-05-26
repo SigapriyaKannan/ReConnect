7f1e87cb578851a587aa746e823008205f635b8d
Vineeth Gudipalli <vn388445@dal.ca>
2024-05-26 17:46:44 -0300

    feat: Added toast module

--- frontend/Re-Connect/src/app/app.component.ts
+++ frontend/Re-Connect/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'rc-root',
   standalone: true,
-  imports: [RouterOutlet],
+  providers: [MessageService],
+  imports: [RouterOutlet, ToastModule],
   template: `
     <router-outlet />
+    <p-toast position="bottom-right"/>
   `,
   styles: [],
 })
