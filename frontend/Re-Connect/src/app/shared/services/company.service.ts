3daeed3ab3f05d281a7edde3030382c948388290
Vineeth Gudipalli <vn388445@dal.ca>
2024-05-26 15:32:48 -0300

    feat: Added shared services

---
+++ frontend/Re-Connect/src/app/shared/services/company.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CompanyService {
+
+  constructor(private http: HttpClient) { }
+
+  getCompanies() {
+    return this.http.get("");
+  }
+}
