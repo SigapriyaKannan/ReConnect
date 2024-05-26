6039690ea7bd5ad335af8ae38ed9ff30073a99d2
Vineeth Gudipalli <vn388445@dal.ca>
2024-05-25 23:00:08 -0300

    feat(sign-up): Initialized Sign Up component

---
+++ frontend/Re-Connect/src/app/components/shared/signup/signup.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rc-signup',
+  standalone: true,
+  imports: [],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.scss'
+})
+export class SignupComponent {
+
+}
