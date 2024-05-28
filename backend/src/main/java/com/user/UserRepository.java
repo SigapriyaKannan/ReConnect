144b8690c1656ed100a33cdf53d8585b40e1f55d
mansikalathiya <125549487+mansikalathiya@users.noreply.github.com>
2024-05-28 13:04:36 -0300

    Added JwtAuthenticationFilter file and JWtService file and add dependency in pom.xml

---
+++ backend/src/main/java/com/user/UserRepository.java
@@ -0,0 +1,13 @@
+package com.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+
+
+}
