b0f9d0cdad5f1bbd0e5828f87906b48de0cdb372
Vineeth Gudipalli <vn388445@dal.ca>
2024-06-07 14:12:44 -0300

    feat(CMS): Added CMS components

---
+++ frontend/Re-Connect/src/app/cms/components/admin-companies/admin-companies.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rc-admin-companies',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-companies.component.html',
+  styleUrl: './admin-companies.component.scss'
+})
+export class AdminCompaniesComponent {
+
+}
