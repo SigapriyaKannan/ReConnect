0083abf44c4b2016f719719ce001f1e3217b35d1
Vineeth Gudipalli <vn388445@dal.ca>
2024-06-14 19:11:28 -0300

    Added Country endpoints for getAllCountries, getCountry/{countryId}, addCountry

---
+++ backend/src/main/java/com/dal/asdc/reconnect/DTO/Helper/CountryDTO.java
@@ -0,0 +1,11 @@
+package com.dal.asdc.reconnect.DTO.Helper;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CountryDTO {
+    private int countryId;
+    private String countryName;
+}
