66a9282520eb561efd9059e12eec247cf5927838
mansikalathiya <125549487+mansikalathiya@users.noreply.github.com>
2024-07-03 21:12:45 -0300

    Search backend

--- backend/src/main/java/com/dal/asdc/reconnect/repository/UserDetailsRepository.java
+++ backend/src/main/java/com/dal/asdc/reconnect/repository/UserDetailsRepository.java
@@ -1,8 +1,14 @@
 package com.dal.asdc.reconnect.repository;
+import com.dal.asdc.reconnect.model.Company;
 import com.dal.asdc.reconnect.model.UserDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserDetailsRepository extends JpaRepository<UserDetails, Integer>
 {
+    @Query("SELECT userName FROM UserDetails WHERE company = :company")
+    List<String> findUsernamesByCompany(Company company);
 
 }
