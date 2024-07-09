2c44af00b2823457ed01d9ea0272cf1248661a2a
mansikalathiya <125549487+mansikalathiya@users.noreply.github.com>
2024-07-09 14:43:42 -0300

    users page

--- backend/src/main/java/com/dal/asdc/reconnect/repository/UsersRepository.java
+++ backend/src/main/java/com/dal/asdc/reconnect/repository/UsersRepository.java
@@ -1,10 +1,21 @@
 package com.dal.asdc.reconnect.repository;
+import com.dal.asdc.reconnect.DTO.Users.User;
+import com.dal.asdc.reconnect.model.UserType;
 import com.dal.asdc.reconnect.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UsersRepository extends JpaRepository<Users, Integer>
 {
     Users findByUserEmail(String email);
     Users findByResetToken(String resetToken);
 
+    @Query(value = "SELECT ud.UserName, u.TypeID FROM Users u " +
+            "JOIN UserDetails ud ON u.UserID = ud.UserID " +
+            "JOIN UserType ut ON u.TypeID = ut.TypeID " +
+            "WHERE ut.TypeName = :typeName", nativeQuery = true)
+    List<Object[]> findUserNameAndTypeIdByUserType(@Param("typeName") String typeName);
 }
