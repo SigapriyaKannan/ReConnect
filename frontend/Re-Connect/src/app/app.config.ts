edd904bc9702cc72e6356ca0893e7c7a50c390b7
mansikalathiya <125549487+mansikalathiya@users.noreply.github.com>
2024-07-13 14:08:37 -0300

    frontend authentication

--- frontend/Re-Connect/src/app/app.config.ts
+++ frontend/Re-Connect/src/app/app.config.ts
@@ -3,9 +3,10 @@ import "./shared/extensions/form.extension";
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
 import { provideRouter, withHashLocation } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
+import { customInterceptor } from './shared/components/auth/custom.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideAnimationsAsync(), provideRouter(routes, withHashLocation()), provideHttpClient()]
+  providers: [provideAnimationsAsync(), provideRouter(routes, withHashLocation()), provideHttpClient(withInterceptors([customInterceptor]))]
 };
---
+++ frontend/Re-Connect/src/app/shared/components/auth/non-auth-guard.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { AuthService } from './auth-service.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NonAuthGuard implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean {
+    if (this.authService.isLoggedIn()) {
+      this.router.navigate(['/home']); // User is authenticated, redirect to home or dashboard
+      return false;
+    } else {
+      return true; // User is not authenticated, allow access to route
+    }
+  }
+}
