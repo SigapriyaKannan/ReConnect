4b1257a1f5c282fe4fea46da62354be0564ace1c
Vineeth Gudipalli <vn388445@dal.ca>
2024-07-15 21:54:16 -0300

    feat (Build): Dev merge

---
+++ backend/src/main/java/com/dal/asdc/reconnect/DTO/LoginDto/LoginResponse.java
@@ -0,0 +1,18 @@
+package com.dal.asdc.reconnect.dto.LoginDto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class LoginResponse
+{
+
+
+    private int status;
+
+    private String detail;
+
+    private LoginResponseBody body;
+
+}
